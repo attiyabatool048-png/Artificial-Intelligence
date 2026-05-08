d8abad2ecd0f803daccfc05b93a4243e8231d6a4
Attiya Batool <attiyabatool048@gmail.com>
2026-05-08 23:35:51 +0500

    Add files via upload

---
+++ tic tac toe/a.py
@@ -0,0 +1,16 @@
+# ============================================
+# FILE: app.py
+# ============================================
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home Route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Run Flask App
+if __name__ == '__main__':
+    app.run(debug=True)
